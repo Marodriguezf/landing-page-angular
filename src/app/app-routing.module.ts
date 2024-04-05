44cb57637e5acba3e06b5614a728ab3261348c06
Marodriguezf <alejandra.franco1290@gmail.com>
2024-04-04 21:41:16 -0500

    Upload Project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MangaComponent } from './manga/manga.component';
+import { AnimeComponent } from './anime/anime.component';
+import { PersonajesComponent } from './personajes/personajes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component:HomeComponent},
+  {path: 'manga',component:MangaComponent},
+  {path: 'anime',component:AnimeComponent},
+  {path: 'personajes',component:PersonajesComponent},
+  {path:'**',redirectTo:'',pathMatch:'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MangaComponent } from './manga/manga.component';
+import { AnimeComponent } from './anime/anime.component';
+import { PersonajesComponent } from './personajes/personajes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MangaComponent,
+    AnimeComponent,
+    PersonajesComponent
   ],
   imports: [
     BrowserModule,
